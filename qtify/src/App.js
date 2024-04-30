4bbb4e54abfcac462ce1b139d9387cfacba8df48
Nithin <nithin@Nithins-MacBook-Pro.local>
2024-04-30 20:10:36 +0530

    ME_QTIFY_MODULE_NAVBAR

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,39 +1,17 @@
-import { useState, useEffect } from 'react';
-
-import Navbar from './Components/Navbar/Navbar';
-import HeroSection from './Components/HeroSection/HeroSection';
-import CardSection from './Components/CardSection/CardSection';
-import AccordionSection from './Components/AccordionSection/AccordionSection';
-import { getTopSongsData, getNewSongsData, getSongsData } from './utils/util';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Card from "./components/Card";
+import Song from "./components/Song";
 
 function App() {
-  const [topSongsData, setTopSongsData] = useState([]);
-  const [newSongsData, setNewSongsData] = useState([]);
-  const [songsData, setSongsData] = useState([]);
-
-  useEffect(() => {
-    (async () => {
-      const topSongsData = await getTopSongsData();
-      setTopSongsData(topSongsData);
-
-      const newSongsData = await getNewSongsData();
-      setNewSongsData(newSongsData);
-
-      const songsData = await getSongsData();
-      setSongsData(songsData);
-    })();
-  }, []);
   return (
-    <>
-      <div>
-        <Navbar />
-        <HeroSection />
-        <CardSection name="Top Albums" songsData={topSongsData} />
-        <CardSection name="New Albums" songsData={newSongsData} />
-        <CardSection name="Songs" songsData={songsData} showFilters />
-        <AccordionSection />
-      </div>
-    </>
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <Card />
+      <Song />
+    </div>
   );
 }
 
