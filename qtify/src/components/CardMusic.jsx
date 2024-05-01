97c46e7e75369913ac85580e3c94a70fc363792d
Nithin <nithin@Nithins-MacBook-Pro.local>
2024-05-01 23:35:16 +0530

    SONGS

--- qtify/src/components/CardMusic.jsx
+++ qtify/src/components/CardMusic.jsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import { Card, CardContent, CardMedia, Typography, Chip, Tooltip } from '@mui/material';
+import React, { useEffect } from "react";
+import { Card, CardContent, CardMedia, Typography, Chip } from "@mui/material";
 
 const CardMusic = ({ card }) => {
   return (
-    <Tooltip title={`${card.songs.length} songs`} placement="top">
-      <div>
-        <Card>
-          <CardMedia
-            component="img"
-            alt="Album Image"
-            height="140"
-            image={card.image}
+    <div>
+      <Card>
+        <CardMedia
+          component="img"
+          alt="Spotify Image"
+          height="140"
+          image={card.image}
+        />
+        <CardContent>
+          <Chip
+            label={`${
+              card.follows
+                ? `Followers: ${card.follows}`
+                : `Likes: ${card.likes}`
+            }`}
           />
-          <CardContent>
-            <Chip
-              label={card.follows ? `Followers: ${card.follows}` : `Likes: ${card.likes}`}
-            />
-          </CardContent>
-        </Card>
-        <Typography variant="h6">{card.title}</Typography>
-      </div>
-    </Tooltip>
+        </CardContent>
+      </Card>
+      <Typography variant="h6">{card.title}</Typography>
+    </div>
   );
 };
 
